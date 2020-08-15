e0dd6a65af75f76e1ccd984d969aaf485247ee98
tp <tp@tp.tp>
2020-08-15 15:02:20 +0200

    Bugfixes

--- readme.md
+++ readme.md
@@ -1,6 +1,34 @@
-How to setup vim+tmux config on a new machine
-=============================================
-1) mkdir ~/.dotfiles
-2) cd ~/.dotfiles
-3) git clone https://github.com/tp-space/vim-config.git .
-4) ./setup.sh
+# How to setup vim+tmux config on a new machine
+1. mkdir ~/.dotfiles
+1. cd ~/.dotfiles
+1. git clone https://github.com/tp-space/vim-config.git .
+1. ./setup.sh
+
+# Install NVIM from source
+Within an empty folder, do:
+1. git clone https://github.com/neovim/neovim.git
+1. cd neovim
+1. git checkout stable
+1. make CMAKE\_BUILD\_TPYE=RelWithDebInfo
+1. sudo make install
+Major required build tools: autoconf automake cmake g++."
+
+# Launch nvim the first time
+1. Make sure to exit all existing vim, tmux and bash instances
+1. Launch a new bash shell
+1. Launch tmux
+1. Launch nvim
+1. Check if everything is fine with :checkhealth
+1. Install plugin with :PluginInstall
+Now your are all set
+
+# Commit changes to github
+First you need to provide information about the user committing the changes.
+To configure the git settings, enter below command:
+- git config --\<flag\> user.name "\<name\>"
+- git config --\<flag\> user.email "\<email\>"
+The flag can be set to _local_, _global_ or _system_. The default value is _local_.
+
+You can't use nvim to push the repository to github (no iterative session to receive pwd).
+So just type in the bash: git push origin master
+
--- setup.sh
+++ setup.sh
@@ -1,62 +1,102 @@
 #!/bin/bash
 
-TP_PWD=$(pwd)
-TP_NVIM_CONFIG=".config/nvim"
-TP_NVIM_AUTOLOAD="$TP_NVIM_CONFIG/autoload"
-TP_NVIM_CONFIG_PLUGGED="$TP_NVIM_CONFIG/config-plugged"
+# stop if error
+set -e 
 
-all_tool_names=(
-    "nvim", 
-    "tmux", 
-    "python3", 
-    "pip3", 
-    "npm", 
+if [ -z "$HOME" ]; then
+  printf "The \$HOME variable is not set. Can't run the setup"
+  exit 1
+fi
+
+TP_PWD=$(pwd)
+TP_NVIM_CONFIG_SOURCE="$TP_PWD/.config/nvim"
+TP_NVIM_CONFIG_TARGET="$HOME/.config/nvim"
+TP_NVIM_AUTOLOAD="autoload"
+TP_NVIM_PLUGGED="config-plugged"
+TP_NVIM_CONFIG_BASH="bash_config.txt"
+TP_TOOLS_NOK=
+
+tools=(
+    "git"
+    "nvim"
+    "tmux"
+    "python3"
+    "pip3"
+    "node"
+    "npm"
+    "yarn"
     "bash")
-all_tool_msgs=(
-    "From source: git clone https://github.com/neovim/neovim.git.\n 
-    git checkout stable.\n 
-    make CMAKE_BUILD_TPYE=RelWithDebInfo.\n 
-    sudo make install.\n
-    Major required build tools: autoconf automake cmake g++", 
-    "", 
-    "", 
-    "", 
-    "",
-    "Setup assumes presence of bash")
 
 # check if tools are installed
-for i in "${all_tool_names[@]}"; do
-  if ! [ -x "$(command -v nvim)" ]; then
-    echo "$i is not installed. Please install $i"
-    echo "${all_tool_msgs[$i]}"
-    echo "Setup is aborted."
-    exit 1
+printf "Check if all required tools are installed:\n"
+for i in "${!tools[@]}"; do
+  if [ -x "$(command -v ${tools[$i]})" ]; then
+    printf "OK  ${tools[$i]} is installed.\n"
+  else
+    printf "ERR ${tools[$i]} is not installed. Please install ${tools[$i]}.\n"
+    TP_TOOLS_NOK="KO"
   fi
 done
 
-# make sure config folder for nvim exists
-mkdir -p ~/$TP_NVIM_AUTOLOAD
-mkdir -p ~/$TP_NVIM_CONFIG_PLUGGED
+# exit if there is a tool missing
+if ! [ -z "$TP_TOOLS_NOK" ]; then
+  printf "Setup is aborted.\n"
+  exit 1
+fi
+
+printf "All required tools are installed.\n"
+
+# install python bindings
+printf "Installing python3 bindings ...\n"
+pip3 install pynvim
+printf "Python3 bindings have been installed.\n"
+
+# install python bindings
+printf "Installing npm bindings ...\n"
+npm install -g neovim
+printf "Npm bindings have been installed.\n"
+
+# cleanup nvim config folders
+printf "Setting up symbolic links ...\n"
+mkdir -p $TP_NVIM_CONFIG_TARGET
+cd $TP_NVIM_CONFIG_TARGET
+rm -rf $TP_NVIM_AUTOLOAD
+rm -rf $TP_NVIM_PLUGGED
 
 # generate symbolic links for tmux
-ln -s -f -t "~/" "$TP_PWD/.tmux.conf"
+cd $HOME 
+ln -f -s "$TP_PWD/.tmux.conf" 
 
 # generate symbolic links for nvim 
-ln -s -f -t "~/$TP_NVIM_AUTOLOAD" "$TP_PWD/$TP_NVIM_AUTOLOAD"
-ln -s -f -t "~/$TP_NVIM_CONFIG_PLUGGED" "$TP_PWD/$TP_NVIM_CONFIG_PLUGGED"
-ln -s -f -t "~/$TP_NVIM_CONFIG" "$TP_PWD/$TP_NVIM_CONFIG/init.vim"
+#cd ${TP_NVIM_CONFIG_TARGET}
+cd $TP_NVIM_CONFIG_TARGET
+ln -f -s "$TP_NVIM_CONFIG_SOURCE/init.vim" 
+ln -f -s "$TP_NVIM_CONFIG_SOURCE/$TP_NVIM_PLUGGED"
+ln -f -s "$TP_NVIM_CONFIG_SOURCE/$TP_NVIM_AUTOLOAD" 
 
-if [ -f "~/.bashrc" ]; then
+printf "Symbolic link setup completed.\n"
 
-  # adjust bashrc config file
-  tp_source_cmd="source $pwd/bash_config.txt"
-  if [ $(grep -e "$tp_source_cmd" < ~/.bashrc | wc -l ) = 0 ]; then
-    echo $tp_source_cmd >> ~/.bashrc
-    echo "Appended the following line to ~/.bashrc: $tp_source_cmd"
+# adjust bashrc config file
+printf "Updating bash configuration ...\n"
+if [ -f "$HOME/.bashrc" ]; then
+
+  TP_SOURCE_CMD="source \"$TP_PWD/$TP_NVIM_CONFIG_BASH\""
+  if [ $(grep -e "$TP_SOURCE_CMD" < "$HOME/.bashrc" | wc -l ) = 0 ]; then
+    echo "" >> "$HOME/.bashrc"
+    echo "# Import personal bash configuration" >> "$HOME/.bashrc"
+    echo $TP_SOURCE_CMD >> "$HOME/.bashrc"
+    printf "Appended the following line to $HOME/.bashrc: $TP_SOURCE_CMD\n"
+    printf "Bash configuration updated.\n"
+    cd $TP_PWD
+    source $TP_NVIM_CONFIG_BASH
   else
-    echo ".bashrc config already present. Nothing to do"
+    printf ".bashrc is already correctly configured. Nothing to do.\n"
   fi
+  printf "Setup completed successfully.\n"
 
 else
-  echo "Didn't found .bashrc file. Skipped .bashrc config"
+  printf "Didn't find the .bashrc file. Skipped .bashrc config.\n"
 fi
+
+cd $TP_PWD
+
